7ce39f0e1ebf87a2b309a9a5e425eeb9f07e8afc
Asad Ullah Riaz <asadullahriaz7@gmail.com>
2021-08-03 12:36:39 +0500

    Routes Implementation along with Navigation using Link

--- react-router-demo/src/App.js
+++ react-router-demo/src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import { Link, Route, Switch } from 'react-router-dom';
+import Category from './components/Category';
+import Products from './components/Products';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className='App'>
+      <nav className='navbar navbar-light'>
+        <ul className='nav navbar-nav'>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/category'>Category</Link>
+          </li>
+          <li>
+            <Link to='/products'>Products</Link>
+          </li>
+        </ul>
+      </nav>
+      <Route path='/' exact component={Home} />
+      <Route path='/category' component={Category} />
+      <Route path='/products' component={Products} />
     </div>
   );
 }
---
+++ react-router-demo/src/components/Category/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Category = () => (
+  <div>
+    <h2>Category</h2>
+  </div>
+);
+
+export default Category;
